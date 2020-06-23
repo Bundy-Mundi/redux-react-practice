eb5782d65c4a0708c2f5e89df0642102a4f846a8
Ben <bennie.ben095@gmail.com>
2020-06-23 18:12:14 +0900

    #1.3 Subscriptions

--- react-client/src/index.js
+++ react-client/src/index.js
@@ -5,6 +5,7 @@ const number = document.getElementById("number");
 const minusBtn = document.getElementById("minus");
 
 const countModifier = (count = 0, action) => {
+  //console.log(count, action)
   if(action.type === "Add"){
     return count + 1
   }
@@ -18,10 +19,21 @@ const countModifier = (count = 0, action) => {
 
 const countStore = createStore(countModifier);
 
-countStore.dispatch({ type:"Add" });
+// OnChange decides what to do when there is any single updates.
+function onChange() {
+  number.innerText = countStore.getState();
 
-countStore.dispatch({ type:"Add" });
+}
+function handleAdd () {
+  // "type" is an action's type(action.type)
+  countStore.dispatch({ type: "Add" })
+}
+function handleMinus () {
+  // "type" is an action's type(action.type)
+  countStore.dispatch({ type: "Minus" })
+}
+// Subscribe lets us to access to change when store.dispatch is fired
+countStore.subscribe(onChange);
+addBtn.addEventListener("click", handleAdd);
+minusBtn.addEventListener("click", handleMinus);
 
-countStore.dispatch({ type:"Add" });
-
-console.log(countStore.getState());
