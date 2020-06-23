e2dd10516172c4818f79e9a67a95910ca266846a
Ben <bennie.ben095@gmail.com>
2020-06-23 22:20:07 +0900

    Complete Redux to do list

--- react-client/src/index.js
+++ react-client/src/index.js
@@ -1,37 +1,70 @@
 import { createStore } from "redux";
+import "./index.css";
 
-const addBtn = document.getElementById("add");
-const number = document.getElementById("number");
-const minusBtn = document.getElementById("minus");
+const input = document.getElementById("input");
+const form = document.getElementById("form");
+const list = document.getElementById("list");
 
-const ADD = "Add";
-const MINUS = "Minus";
+// Redux Action Type Config
+const ADD = "ADD";
+const DELETE = "DELETE";
 
-const countModifier = (count=0, action) => {
-  //console.log(count, action)
-  switch (action.type){
-    case ADD:
-      return count + 1
-    case MINUS:
-      return count - 1
-    default:
-      return count; 
+// Redux Action Object Config
+const addToDo = (text) => {
+  return { 
+    type: ADD,
+    text
+  }
+};
+const deleteToDo = (id) => {
+  return {
+    type: DELETE,
+    id
   }
 };
 
-const countStore = createStore(countModifier);
+// Redux Config
+const itemListModifier = (itemList = [], action) => {
+  switch (action.type){
+    case ADD:
+      // Return a new array with new data + old data
+      return [...itemList, { text: action.text, id: Date.now() }];
+    case DELETE:
+      const newArray = itemList.filter(item => item.id !== action.id);
+      return newArray;
+    default:
+      return itemList;
+  }
+};
+const itemListStore = createStore(itemListModifier);
+itemListStore.subscribe(paintToDo);
 
-function onChange() {
-  number.innerText = countStore.getState();
-}
-function handleAdd () {
-  countStore.dispatch({ type: ADD })
-}
-function handleMinus () {
-  countStore.dispatch({ type: MINUS })
-}
+// Vanilla JS Functions
+function paintToDo () {
+  const itemList = itemListStore.getState();
+  list.innerHTML = ``;
+  itemList.forEach(item=>{
+    const li = document.createElement("li");
+    const btn = document.createElement("button");
+    li.innerText = item.text;
+    li.id = item.id;
+    btn.innerText = "X";
+    btn.className = "del-btn";
+    btn.addEventListener("click", handleDelete);
+    list.appendChild(li);
+    li.appendChild(btn);
+  })
+};
 
-countStore.subscribe(onChange);
-addBtn.addEventListener("click", handleAdd);
-minusBtn.addEventListener("click", handleMinus);
-
+// Vanilla JS EventHandlers
+function onSubmit (e) {
+  e.preventDefault();
+  const toDo = input.value;
+  input.value = "";
+  itemListStore.dispatch(addToDo(toDo));
+};
+function handleDelete(e){
+  const id = parseInt(e.target.parentNode.id);
+  itemListStore.dispatch(deleteToDo(id));
+};
+form.addEventListener("submit", onSubmit);
