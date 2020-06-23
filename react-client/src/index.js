cf89d00e30441e7bd450bd9984b73441f9be097a
Ben <bennie.ben095@gmail.com>
2020-06-23 18:21:45 +0900

    #1.4 Recap & Code Enhancement

--- react-client/src/index.js
+++ react-client/src/index.js
@@ -4,35 +4,33 @@ const addBtn = document.getElementById("add");
 const number = document.getElementById("number");
 const minusBtn = document.getElementById("minus");
 
-const countModifier = (count = 0, action) => {
+const ADD = "Add";
+const MINUS = "Minus";
+
+const countModifier = (count=0, action) => {
   //console.log(count, action)
-  if(action.type === "Add"){
-    return count + 1
-  }
-  else if(action.type === "Minus"){
-    return count - 1
-  }
-  else if(action.type === "Test"){
-    return "test"
+  switch (action.type){
+    case ADD:
+      return count + 1
+    case MINUS:
+      return count - 1
+    default:
+      return count; 
   }
 };
 
 const countStore = createStore(countModifier);
 
-// OnChange decides what to do when there is any single updates.
 function onChange() {
   number.innerText = countStore.getState();
-
 }
 function handleAdd () {
-  // "type" is an action's type(action.type)
-  countStore.dispatch({ type: "Add" })
+  countStore.dispatch({ type: ADD })
 }
 function handleMinus () {
-  // "type" is an action's type(action.type)
-  countStore.dispatch({ type: "Minus" })
+  countStore.dispatch({ type: MINUS })
 }
-// Subscribe lets us to access to change when store.dispatch is fired
+
 countStore.subscribe(onChange);
 addBtn.addEventListener("click", handleAdd);
 minusBtn.addEventListener("click", handleMinus);
